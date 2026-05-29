011d0b8936fadab298b2dd6586e4a06c2d5d6f97
Karla-gz <karlaguadalupeguzmanvargas4@gmail.com>
2026-05-29 03:14:56 -0600

    agregue nuevos productos a mi tienda

--- marketplace_main/marketplace_main/urls.py
+++ marketplace_main/marketplace_main/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings  # <-- Revisa que tengas esta importación
+from django.conf.urls.static import static  # <-- Y esta también
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
 ]
+
+# Esto es lo que hace la magia para que se vean las fotos en tu compu:
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
