08962e19b7cae58b8c7d5defd11c6b01265be7ff
Karla-gz <karlaguadalupeguzmanvargas4@gmail.com>
2026-05-29 03:31:44 -0600

    Realice cambios en settings.py

--- marketplace_main/marketplace_main/settings.py
+++ marketplace_main/marketplace_main/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,10 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    
     'store',
 ]
-
 
 AUTH_USER_MODEL = 'store.User'
 
@@ -123,9 +123,9 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-import os
 
+
+# 🚨 CONFIGURACIÓN DE ARCHIVOS MULTIMEDIA (IMÁGENES DE PRODUCTOS)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
